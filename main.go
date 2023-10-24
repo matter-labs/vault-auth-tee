d60b17e20f8d1c11c07a43a6bedd3775de46e623
Harald Hoyer <harald@hoyer.xyz>
2023-10-24 09:38:58 +0200

    feat: initial commit
    Signed-off-by: Harald Hoyer <harald@matterlabs.dev>

---
+++ main.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// Copyright (c) Matter Labs
+
+package main
+
+import (
+	"os"
+
+	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
+
+	"github.com/matter-labs/vault-auth-tee/tee"
+)
+
+func main() {
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	if err := plugin.ServeMultiplex(&plugin.ServeOpts{
+		BackendFactoryFunc: tee.Factory,
+		// set the TLSProviderFunc so that the plugin maintains backwards
+		// compatibility with Vault versions that don’t support plugin AutoMTLS
+		TLSProviderFunc: tlsProviderFunc,
+	}); err != nil {
+		logger := hclog.New(&hclog.LoggerOptions{})
+
+		logger.Error("plugin shutting down", "error", err)
+		os.Exit(1)
+	}
+}
---
+++ tee/path_info.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// Copyright (c) Matter Labs
+
+package tee
+
+import (
+	"context"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+
+	"github.com/matter-labs/vault-auth-tee/version"
+)
+
+func pathInfo(b *backend) *framework.Path {
+	return &framework.Path{
+		Pattern:      "info",
+		HelpSynopsis: "Display information about the plugin",
+		HelpDescription: `
+
+Displays information about the plugin, such as the plugin version and where to
+get help.
+
+`,
+		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.ReadOperation: b.pathInfoRead,
+		},
+	}
+}
+
+// pathInfoRead corresponds to READ auth/tee/info.
+func (b *backend) pathInfoRead(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"name":    version.Name,
+			"version": version.Version,
+		},
+	}, nil
+}
